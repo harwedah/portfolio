b2e2ed6612748c3e681b18ccbb16ce85d29abadd
harwedah <alabiaweda619@gmail.com>
2023-12-16 23:03:24 -0800

    commit

---
+++ port1.js
@@ -0,0 +1,9 @@
+function load() {
+    document.images['img1'].src = '/DiR/Images/img1.PNG'
+}
+function mouseOver() {
+    document.images['img1'].src = '/DiR/Images/img2.PNG'
+}
+function mouseOut() {
+    document.images['img1'].src = '/DiR/Images/img1.PNG'
+}
